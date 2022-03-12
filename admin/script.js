830a2387bc9829d039de832a1a80b45761a6ed44
Chris Lopez <73512428+chris-lop@users.noreply.github.com>
2022-03-11 21:40:46 -0500

    Bug Fix - Stray End Tags
    Removed Stray Input End Tags in adminSocial page

--- admin/script.js
+++ admin/script.js
@@ -67,8 +67,8 @@ function readMessages() {
     var uploadFile = document.getElementById("uploadFile").files[0];
     var fileReader = new FileReader();
     fileReader.onload = function(fileLoadedEvent) {
-        var elemHdl = document.getElementById("displayWin");
-        elemHdl.value = fileLoadedEvent.target.result;
+        var temp = document.getElementById("displayWin");
+        temp.value = fileLoadedEvent.target.result;
     };
     fileReader.readAsText(uploadFile, "UTF-8");
 };
